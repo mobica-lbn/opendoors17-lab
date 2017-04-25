665d72c506b859678112b884b0f02351fa5f63fc
szwa <szymon.walczak@mobica.com>
2017-04-25 14:50:38 +0200

    change properties and tests

--- DynamoDB/src/test/java/com/mobica/cloud/aws/dynamo/DynamoDBApplicationTests.java
+++ DynamoDB/src/test/java/com/mobica/cloud/aws/dynamo/DynamoDBApplicationTests.java
@@ -19,7 +19,7 @@ public class DynamoDBApplicationTests {
 
   @Test
   public void getItem() {
-    assertNotNull(service.findById(-1));
+    assertNotNull(service.findById(1));
   }
 
   @Test
@@ -29,7 +29,7 @@ public class DynamoDBApplicationTests {
 
   @Test
   public void getAttribute() {
-    assertTrue(service.findById(-1).hasAttribute("title"));
+    assertTrue(service.findById(1).hasAttribute("title"));
   }
 
   @Test
